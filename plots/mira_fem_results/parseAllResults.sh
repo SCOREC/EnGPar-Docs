367c86b5b9249dc4ac781cf789ded8af785c4481
Cameron Smith <smithc11@rpi.edu>
2018-08-30 12:52:46 -0400

    plots: bash array index -1 did not work on osx

--- plots/mira_fem_results/parseAllResults.sh
+++ plots/mira_fem_results/parseAllResults.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 dir=results$1
+[ ! -e $dir ] && mkdir $dir
 echo "#Cores ParMA EnGPar">$dir/time_v_cores.dat
 echo "#Cores ParMA EnGPar">$dir/gd_v_cores.dat
 echo "#Cores ParMA EnGPar">$dir/migrate_v_cores.dat
@@ -11,8 +12,11 @@ echo "#Cores Original ParMA EnGPar">$dir/numvtx_v_cores.dat
 for i in {128,256,512}
 do
     p_outputs=(parma/$i/*.output)
+    len=${#p_outputs[@]}; p_last=$((len-1))
     e_outputs=(trial$1/$i/engpar/timing/*.output) 
-    ./parseResults.sh ${p_outputs[-1]} ${e_outputs[-1]} $i $dir
+    len=${#e_outputs[@]}; e_last=$((len-1))
+    ./parseResults.sh ${p_outputs[$p_last]} ${e_outputs[$e_last]} $i $dir
     e2_outputs=(trial$1/$i/engpar/timing/*.output)
-    ./parseTime.sh ${p_outputs[-1]} ${e_outputs[-1]} ${e2_outputs[-1]} $i $dir
+    len=${#e2_outputs[@]}; e2_last=$((len-1))
+    ./parseTime.sh ${p_outputs[$p_last]} ${e_outputs[$e_last]} ${e2_outputs[$e2_last]} $i $dir
 done
